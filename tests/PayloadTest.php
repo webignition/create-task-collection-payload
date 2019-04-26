105fedb0c135e7b0d3dc3f168f70b9840e5517bb
Jon Cram <webignition@gmail.com>
2019-04-26 12:47:42 +0100

    Add Payload::getJobIdentifier(), ::getTaskPayloads() (#8)

--- tests/PayloadTest.php
+++ tests/PayloadTest.php
@@ -10,6 +10,24 @@ use webignition\Uri\Uri;
 
 class PayloadTest extends TestCase
 {
+    public function testGetJobIdentifier()
+    {
+        $jobIdentifier = 'x123';
+
+        $payload = new Payload($jobIdentifier, []);
+
+        $this->assertSame($jobIdentifier, $payload->getJobIdentifier());
+    }
+
+    public function testGetTaskPayloads()
+    {
+        $taskPayload = new TaskPayload(new Uri('http://example.com/'), TypeInterface::HTML_VALIDATION, '');
+
+        $payload = new Payload('x123', [$taskPayload]);
+
+        $this->assertSame([$taskPayload], $payload->getTaskPayloads());
+    }
+
     public function testJsonSerialize()
     {
         $jobIdentifier = 'x123';
